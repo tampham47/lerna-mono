b334d31d02a98aca3881096cca9edb4109887b8a
Tam Pham <tam.pham@doltech.vn>
2019-09-09 15:34:04 +0700

    init project

---
+++ packages/app-react/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Button from '@doltech/shared-components/lib/Button';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button>123</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
